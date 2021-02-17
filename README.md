fdd5858bb6309ea77c52c13f7d7e1d99d154ccb7
Romy van Drie <romy.vandrie@tno.nl>
2021-02-17 11:14:09 +0000

    split flintfiller package into two packages: juridecomposer and flintfiller. Juridecomposer puts the text from the xml in a dataframe, flintfiller analyses the text for creating a flint frame. The main method in src calls both package together, like the previous version of flintfiller.
    add small updates from TNO internal code

--- README.md
+++ README.md
@@ -15,13 +15,20 @@ and follow the following steps:
 - `poetry install`
 
 - Go to project settings of your editor and set the created virtual environment as project interpreter
+- **Important:** this tool uses python 3.6 because the pattern package, which is used for the part-of-speech tagging, 
+only supports python 3.6. Running FlintFiller with a higher python version results in the following error:
+```
+packages\pattern\text\__init__.py", line 625, in <genexpr>
+    dict.update(self, (x.split(" ")[:2] for x in _read(self._path) if len(x.split(" ")) > 1))
+RuntimeError: generator raised StopIteration
+```
 
-#### Commandline usage of flintfiller
+#### Commandline usage of FlintFiller
 
 After installation of the dependencies through poetry you can enter the following example command:
 
 ``` bash
-python flintfiller/flintfiller.py 
+python src/main.py 
 -x data/xml_files/BWBR0011823_2019-02-27_(Vreemdelingenwet).xml 
 -d data/json_files/BWBR0011823_2019-02-27_(Vreemdelingenwet).json 
 -df data/csv_files/BWBR0011823_2019-02-27_Vreemdelingenwet.csv 
@@ -39,6 +46,13 @@ with arguments:
 Arguments are not required, for instance you can pass only an xml and a location for your dictionary file and it will
 parse the xml to a dictionary. If you already have a POS tagged dataframe in the right format, you can also pass that,
 and pass a location for the flint frames output. 
+
+By running main, you are running both JuriDecompose and FlintFiller. JuriDecompose and FlintFiller are independently 
+callable too (with the same arguments as src/main):
+``` bash
+python flintfiller/flintfiller.py 
+python juridecompose/juridecompose.py 
+```
 
 For more technical information see the technical-README.md
 
@@ -69,6 +83,11 @@ For more information see https://www.forumstandaardisatie.nl/sites/bfs/files/Jur
 
 The JSON flint frames can be opened using Visual Studio Code with the FlintEditor plugin.
 Otherwise it can be opened using Text Editors such as Notepad++.
+
+## Documentation
+
+A more thorough theoretical description of FlintFiller can be found in the [TNO FlintFiller report](./TNO_FlintFiller_report.pdf)
+(in Dutch only)
 
 ## Maintainers
 Roos Bakker, TNO (roos.bakker@tno.nl)
---
+++ src/flintfiller/flintfiller.py
@@ -0,0 +1,79 @@
+"""
+    Copyright (C) 2020 Nederlandse Organisatie voor Toegepast Natuur-
+    wetenschappelijk Onderzoek TNO / TNO, Netherlands Organisation for
+    applied scientific research
+
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+    @author: Maaike de Boer, Roos Bakker
+    @contact: maaike.deboer@tno.nl, roos.bakker@tno.nl
+"""
+
+import argparse
+import sys
+import pandas as pd
+
+import src.flintfiller.chunk_tag_dataframe as chunk_tag_dataframe
+import src.flintfiller.dataframe_to_frame_parser as dataframe_to_frame_parser
+
+
+def read_csv_to_df(csv_file):
+    dataframe = pd.read_csv(csv_file)
+    return dataframe
+
+
+def flintfiller():
+    args = parse_commandline_arguments()
+
+    if args.df_file and args.pt_file:
+        input_df_file = args.df_file
+        input_df = read_csv_to_df(input_df_file)
+        output_pt = args.pt_file
+        chunk_tag_dataframe.parser('pattern', input_df, output_pt)
+        if args.flint_output:
+            input_pt_df = args.pt_file
+            output_flint = args.flint_output
+            dataframe_to_frame_parser.dataframe_to_frame_parser(input_pt_df, output_flint)
+            print(f'finished with creating FLINT frames from dataframe, check file: ' + output_flint)
+        else:
+            print(f'finished with parsing xml to POS tagged dataframe, check file: ' + output_pt)
+
+    elif args.flint_output and args.pt_file and not args.df_file:
+        input_pt_df = args.pt_file
+        output_flint = args.flint_output
+        dataframe_to_frame_parser.dataframe_to_frame_parser(input_pt_df, output_flint)
+        print(f'finished with creating FLINT frames from dataframe, check file: ' + output_flint)
+
+    else:
+        print('you did not pass the right combination of arguments, parsing not possible.')
+
+
+def parse_commandline_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-x', '--xml',
+                        help="input xml file")
+    parser.add_argument('-d', '--dict_file',
+                        help="location of file with dictionary of xml")
+    parser.add_argument('-df', '--df_file',
+                        help="location of file with dataframe of xml")
+    parser.add_argument('-pt', '--pt_file',
+                        help="location of dataframe with pos tags")
+    parser.add_argument('-fo', '--flint_output',
+                        help="Output file with Flint frames.")
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    flintfiller()
